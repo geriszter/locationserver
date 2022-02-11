decf15399ec188d7d3afa7ebce01c9a347bb73a0
GERGO CSONKA <G.CSONKA-2018@hull.ac.uk>
2022-02-11 15:06:21 +0000

    Add project files.

---
+++ locationserver/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+public class locationserver
+{
+    static void Main(string[] args)
+    {
+        runServer();
+    }
+    static void runServer()
+    {
+        TcpListener listener;
+        Socket connection;
+        NetworkStream socketStream;
+        try
+        {
+            listener = new TcpListener(IPAddress.Any, 43);
+            listener.Start();
+            Console.WriteLine("Server started");
+            while (true)
+            {
+                connection = listener.AcceptSocket();
+                socketStream = new NetworkStream(connection);
+                doRequest(socketStream);
+                socketStream.Close();
+                connection.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exeption: " + e.ToString());
+        }
+    }
+    static void doRequest(NetworkStream socketStream)
+    {
+        try
+        {
+
+            StreamWriter sw = new StreamWriter(socketStream);
+            StreamReader sr = new StreamReader(socketStream);
+            Console.WriteLine(sr.ReadToEnd());
+        }
+        catch
+        {
+            Console.WriteLine("Connection faild");
+        }
+    }
+}
+
