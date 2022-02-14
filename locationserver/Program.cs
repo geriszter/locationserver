0960b69b11b842735a55640fa2fb50b68575aca0
GERGO CSONKA <G.CSONKA-2018@hull.ac.uk>
2022-02-14 18:20:04 +0000

    single is done

--- locationserver/Program.cs
+++ locationserver/Program.cs
@@ -2,6 +2,9 @@
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
 public class locationserver
 {
     static void Main(string[] args)
@@ -15,6 +18,7 @@ public class locationserver
         NetworkStream socketStream;
         try
         {
+            Dictionary<string, string> personLocation = new Dictionary<string, string>();
             listener = new TcpListener(IPAddress.Any, 43);
             listener.Start();
             Console.WriteLine("Server started");
@@ -22,7 +26,7 @@ public class locationserver
             {
                 connection = listener.AcceptSocket();
                 socketStream = new NetworkStream(connection);
-                doRequest(socketStream);
+                doRequest(socketStream, personLocation);
                 socketStream.Close();
                 connection.Close();
             }
@@ -32,7 +36,7 @@ public class locationserver
             Console.WriteLine("Exeption: " + e.ToString());
         }
     }
-    static void doRequest(NetworkStream socketStream)
+    static void doRequest(NetworkStream socketStream, Dictionary<string, string> personLocation)
     {
         try
         {
@@ -41,16 +45,40 @@ public class locationserver
             StreamReader sr = new StreamReader(socketStream);
             //Console.WriteLine(sr.ReadToEnd());
 
-            string line = sr.ReadLine().Trim();
+            string line = sr.ReadLine().Trim('\"');
+            line = line.Replace("\"","");
             string[] commands = line.Split(" ");
 
             if (commands.Length == 1)
             {
-
+                if (personLocation.ContainsKey(commands[0]))
+                {
+                    sw.WriteLine(personLocation[commands[0]]);
+                    sw.Flush();
+                    Console.WriteLine($"\"GET {commands[0]}\" OK");
+                }
+                else 
+                {
+                    sw.WriteLine("Error: No data");
+                    sw.Flush();
+                    Console.WriteLine($"\"GET {commands[0]}\" UNKNOWN");
+                }
             }
-            else if(commands.Length == 2)
+            else if(commands.Length > 1)
             {
+                if (personLocation.ContainsKey(commands[0]))
+                {
 
+                    personLocation[commands[0]] = commands[1];
+
+                }
+                else
+                {
+                    personLocation.Add(commands[0],commands[1]);
+                }
+                sw.WriteLine("OK");
+                sw.Flush();
+                Console.WriteLine($"\"PUT {commands[0]} {commands[1]}\" OK");
             }
         }
         catch
