cf754a8baf55715b3ccc8063cccb8be90ff0069a
GERGO CSONKA <G.CSONKA-2018@hull.ac.uk>
2022-02-15 16:43:53 +0000

    works

--- locationserver/Program.cs
+++ locationserver/Program.cs
@@ -41,19 +41,22 @@ public class locationserver
     {
         try
         {
+            socketStream.ReadTimeout = 1000;
+            socketStream.WriteTimeout = 1000;
 
             StreamWriter sw = new StreamWriter(socketStream);
             StreamReader sr = new StreamReader(socketStream);
             //Console.WriteLine(sr.ReadToEnd());
 
             string line = sr.ReadLine().Trim();
-            string[] commands = line.Split("\"");
+            string[] commands = line.Split(" ");
             commands[0] = commands[0].Trim();
             if (commands.Length == 1)
             {
                 if (personLocation.ContainsKey(commands[0]))
                 {
-                    sw.WriteLine(personLocation[commands[0]]);
+                    string location = personLocation[commands[0]];
+                    sw.WriteLine(location);
                     sw.Flush();
                     Console.WriteLine($"\"GET {commands[0]}\" OK");
                 }
@@ -66,19 +69,23 @@ public class locationserver
             }
             else if(commands.Length > 1)
             {
+                string locationstring = "";
+                for (int i = 1; i < commands.Length; i++)
+                {
+                    locationstring += commands[i] + " ";
+                }
+                locationstring = locationstring.Trim();
                 if (personLocation.ContainsKey(commands[0]))
                 {
-
-                    personLocation[commands[0]] = commands[1];
-
+                    personLocation[commands[0]] = locationstring;
                 }
                 else
                 {
-                    personLocation.Add(commands[0],commands[1]);
+                    personLocation.Add(commands[0], locationstring);
                 }
                 sw.WriteLine("OK");
                 sw.Flush();
-                Console.WriteLine($"\"PUT {commands[0]} {commands[1]}\" OK");
+                Console.WriteLine($"\"PUT {commands[0]} {locationstring}\" OK");
             }
         }
         catch
